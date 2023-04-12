beadcb23a4eaf8a040bc73968dd713bed41789d2
Antoni Nowalski <antoninowalski@gmail.com>
2023-04-12 14:27:30 +0200

    TestyJednostkowe

---
+++ MyProject.Tests/ListHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyProject.Tests
+{
+    public class ListHelperTests
+    {
+        
+        [Fact]
+       
+        public void Tests()
+        {
+            
+
+        }
+    }
+}
---
+++ MyProject.Tests/StringBuilderTessts1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Exercise;
+using FluentAssertions;
+
+namespace MyProject.Tests
+{
+    public class StringBuilderTessts1
+    {
+        [Theory]
+        [InlineData("Ala ma kota", "kota ma Ala")]
+        [InlineData("Kot Kasi", "Kasi Kot")]
+        [InlineData("Samolot lata", "lata Samolot")]
+        public void ReverseWords_ForTwoGivenStrings_ReturnsAppendedString(string napis, string excepted)
+        {   
+            //act
+            napis = StringHelper.ReverseWords(napis);
+            //assert
+            //Assert.Equal(napis, result);
+            napis.Should().Be(excepted);
+        }
+        [Fact]
+        public void IsPalindrome_ForAWord_REturnsBoolValue()
+        {
+            //arange
+            var napis = "kajak";
+
+            //act
+            var result = StringHelper.IsPalindrome(napis);
+
+            //assert
+            //Assert.True(result);
+            result.Should().BeTrue();
+        }
+
+    }
+}
